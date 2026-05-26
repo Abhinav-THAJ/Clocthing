4d7d4ff89f3fdd088909ec20732147a3e8a3aa98
Abhinav-THAJ <98870669+Abhinav-THAJ@users.noreply.github.com>
2026-05-26 23:27:34 +0530

    fix: type SmoothScrollProvider easing param and remove deprecated Lenis options

--- src/components/providers/SmoothScrollProvider.tsx
+++ src/components/providers/SmoothScrollProvider.tsx
@@ -11,15 +11,8 @@ export default function SmoothScrollProvider({
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-      direction: "vertical", // vertical, horizontal
-      gestureDirection: "vertical", // vertical, horizontal, both
-      smooth: true,
-      mouseMultiplier: 1,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      infinite: false,
-    } as any);
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
 
     function raf(time: number) {
       lenis.raf(time);
